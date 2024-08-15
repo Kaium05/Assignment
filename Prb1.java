238985254f660b87682c8fbde06712775bea64d2
A Kaium <135166601+Kaium05@users.noreply.github.com>
2024-08-15 18:29:19 +0600

    Add files via upload
    For Hello world

---
+++ Prb1.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.assignment;
+
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        
+        // Prints "Hello, World!" to the console
+        
+        
+        System.out.println("Hello, World!");
+    
+      }
+}
